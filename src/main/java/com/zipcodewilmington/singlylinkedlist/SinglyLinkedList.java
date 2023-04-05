b7539a38140411c084c0752d61abdf1dc04fdc97
timothy <pimtarka@gmail.com>
2023-04-05 17:13:55 -0400

    added tests and methods

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,5 +3,135 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
+
+    private Node<T> head; //null cause I didnt make an instance of the object aka Node node = new Node(info);
+
+    // Node class representing each element in the singly linked list
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+
+        Node(T value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    public void add(T value) {
+        Node<T> newNode = new Node<T>(value);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> currentNode = head;
+            while (currentNode.next != null) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = newNode;
+        }
+    }
+
+    public void remove(T value) {
+        if (head == null) return;
+
+        if (head.value == value) {
+            head = head.next;
+            return;
+        }
+
+        Node<T> currentNode = head;
+        while (currentNode.next != null) {
+            if (currentNode.next.value == value) {
+                currentNode.next = currentNode.next.next;
+                return;
+            }
+            currentNode = currentNode.next;
+        }
+    }
+
+    public boolean contains(T value) {
+        Node<T> currentNode = head;
+
+        while (currentNode != null) {
+            if (currentNode.value == value) {
+                return true;
+            }
+            currentNode = currentNode.next;
+        }
+
+        return false;
+    }
+
+    public int find(T value) {
+        Node<T> currentNode = head;
+        int index = 0;
+
+        while (currentNode != null) {
+            if (currentNode.value == value) {
+                return index;
+            }
+            currentNode = currentNode.next;
+            index++;
+        }
+
+        return -1;
+    }
+
+    public int size() {
+        Node<T> currentNode = head;
+        int count = 0;
+
+        while (currentNode != null) {
+            count++;
+            currentNode = currentNode.next;
+        }
+
+        return count;
+    }
+
+    public int get(int index) {
+        Node<T> currentNode = head;
+        while (currentNode != null) {
+
+        }
+        return 0;
+    }
+
+    public void print() {
+        Node<T> currentNode = head;
+
+        while (currentNode != null) {
+            System.out.print(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        SinglyLinkedList listy = new SinglyLinkedList();
+
+        listy.add(9);
+        listy.add(8);
+        listy.add(7);
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        list.print();
+
+        list.remove(3);
+
+        list.print();
+
+        System.out.println("Contains 3? " + list.contains(3));
+        System.out.println("Contains 2? " + list.contains(2));
+        System.out.println("Index of number? " + list.find(2));
+        System.out.println("How large is the list? " + list.size());
+        System.out.println("Value at index: " + list.get(2));
+    }
 }
+
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,110 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    SinglyLinkedList<String> list;
+
+    @Before
+    public void before() {
+        list = new SinglyLinkedList<String>();
+    }
+
+    @Test
+    public void testAdd() {
+        String expected = "Tim";
+
+        list.add(expected);
+
+        Assert.assertTrue(list.contains(expected));
+    }
+    @Test
+    public void testRemove() {
+        String expected = "Tim";
+        String actual = "Park";
+
+        list.add(expected);
+        list.add(actual);
+        list.remove(actual);
+
+        Assert.assertFalse(list.contains(actual));
+    }
+    @Test
+    public void testContains() {
+        String expected = "Tim";
+
+        list.add(expected);
+
+        Assert.assertTrue(list.contains(expected));
+    }
+    @Test
+    public void testFind() {
+        int expected = 0;
+
+        list.add("Tim");
+
+        Assert.assertEquals(expected, list.find("Tim"));
+    }
+    @Test
+    public void testFind1() {
+        int expected = -1;
+
+        list.add("Tim");
+        list.add("Park");
+
+        int actual = list.find("Park");
+
+        Assert.assertNotEquals(expected, actual);
+    }
+    @Test
+    public void testSize() {
+        int expected = 3;
+        list.add("");
+        list.add("");
+        list.add("");
+
+        int actual = list.size();
+
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testGet() {
+        String expected = "Tim";
+        list.add("");
+        list.add(expected);
+
+        int actual = list.get(1).value;
+
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testCopy() {
+        list.add("Tim");
+        SinglyLinkedList<String> expected = list.copy();
+
+        Assert.assertNotEquals(list.toString(), expected.toString());
+        for (int i = 0; i < list.size(); i++) {
+            Assert.assertEquals(list.get(i).value, expected.get(i).value);
+        }
+    }
+    @Test
+    public void testSort() {
+        list.add("Tim");
+        list.add("Park");
+        list.add("Ace");
+
+        list.sort();
+
+        Assert.assertEquals(list.get(0).value, "Ace");
+        Assert.assertEquals(list.get(1).value, "Tim");
+        Assert.assertEquals(list.get(2).value, "Parm");
+
+
+    }
+
 }
